b2503e805e6418ff4e065aafb254a78b4344bf84
root <root@Gaurav_labs-NetScape_Labs.localdomain>
2020-03-25 10:59:55 +0530

    isenrolled

--- execute.sh
+++ execute.sh
@@ -1,5 +1,5 @@
 cd /var/www/dev/tourney 
 git pull origin development-0.0.1
-chmod -R 0777 /var/www/dev/tourney/storage /var/www/dev/tourney/bootstrap  
+sudo chmod -R 0777 /var/www/dev/tourney/storage /var/www/dev/tourney/bootstrap  
 exit
 
