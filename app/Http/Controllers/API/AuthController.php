330e3a9df0b417a54cc127b485baa15b0a0f98f9
root <root@Gaurav_labs-NetScape_Labs.localdomain>
2020-04-18 15:43:06 +0530

    prize

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -136,7 +136,7 @@ class AuthController extends ApiController {
         try {
             $user = \App\User::findOrFail(\Auth::id());
             $model = new \App\Tournament();
-            $model = $model->select('id', 'name', 'image', 'location', 'price', 'description', 'start_date', 'end_date', 'rules', 'privacy_policy');
+            $model = $model->select('id', 'name', 'image', 'location', 'price', 'description', 'start_date', 'end_date', 'rules', 'privacy_policy','prize_name','prize_image');
             $model = $model->where('state', '1');
             $model = $model->where('end_date','>',\Carbon\Carbon::now());
             $perPage = isset($request->limit) ? $request->limit : 20;
