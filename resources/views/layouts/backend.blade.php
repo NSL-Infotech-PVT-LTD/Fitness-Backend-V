a3646d6e6f0f2e51455706af0a1156bf81d1405f
root <root@Gaurav_labs-NetScape_Labs.localdomain>
2020-05-30 21:48:08 +0530

    api

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -63,110 +63,120 @@
                 </ul>
 
                 <!-- SEARCH FORM -->
-<!--                <form class="form-inline ml-3">
-                    <div class="input-group input-group-sm">
-                        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-                        <div class="input-group-append">
-                            <button class="btn btn-navbar" type="submit">
-                                <i class="fas fa-search"></i>
-                            </button>
-                        </div>
-                    </div>
-                </form>-->
+                <!--                <form class="form-inline ml-3">
+                                    <div class="input-group input-group-sm">
+                                        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                                        <div class="input-group-append">
+                                            <button class="btn btn-navbar" type="submit">
+                                                <i class="fas fa-search"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>-->
 
                 <!-- Right navbar links -->
-<!--                <ul class="navbar-nav ml-auto">
-                     Messages Dropdown Menu 
-                    <li class="nav-item dropdown">
-                        <a class="nav-link" data-toggle="dropdown" href="#">
-                            <i class="far fa-comments"></i>
-                            <span class="badge badge-danger navbar-badge">3</span>
+                <ul class="navbar-nav ml-auto">
+                    <!--Messages Dropdown Menu--> 
+                    <!--                    <li class="nav-item dropdown">
+                                            <a class="nav-link" data-toggle="dropdown" href="#">
+                                                <i class="far fa-comments"></i>
+                                                <span class="badge badge-danger navbar-badge">3</span>
+                                            </a>
+                                            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                                                <a href="#" class="dropdown-item">
+                                                     Message Start 
+                                                    <div class="media">
+                                                        <img src="{{ asset('adminLte/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle') }}">
+                                                        <div class="media-body">
+                                                            <h3 class="dropdown-item-title">
+                                                                Brad Diesel
+                                                                <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                                                            </h3>
+                                                            <p class="text-sm">Call me whenever you can...</p>
+                                                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                                                        </div>
+                                                    </div>
+                                                     Message End 
+                                                </a>
+                                                <div class="dropdown-divider"></div>
+                                                <a href="#" class="dropdown-item">
+                                                     Message Start 
+                                                    <div class="media">
+                                                        <img src="{{ asset('adminLte/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3') }}">
+                                                        <div class="media-body">
+                                                            <h3 class="dropdown-item-title">
+                                                                John Pierce
+                                                                <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+                                                            </h3>
+                                                            <p class="text-sm">I got your message bro</p>
+                                                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                                                        </div>
+                                                    </div>
+                                                     Message End 
+                                                </a>
+                                                <div class="dropdown-divider"></div>
+                                                <a href="#" class="dropdown-item">
+                                                     Message Start 
+                                                    <div class="media">
+                                                        <img src="{{ asset('adminLte/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3') }}">
+                                                        <div class="media-body">
+                                                            <h3 class="dropdown-item-title">
+                                                                Nora Silvester
+                                                                <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+                                                            </h3>
+                                                            <p class="text-sm">The subject goes here</p>
+                                                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                                                        </div>
+                                                    </div>
+                                                     Message End 
+                                                </a>
+                                                <div class="dropdown-divider"></div>
+                                                <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+                                            </div>
+                                        </li>-->
+                    <!--Notifications Dropdown Menu--> 
+                    <!--                    <li class="nav-item dropdown">
+                                            <a class="nav-link" data-toggle="dropdown" href="#">
+                                                <i class="far fa-bell"></i>
+                                                <span class="badge badge-warning navbar-badge">15</span>
+                                            </a>
+                                            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                                                <span class="dropdown-item dropdown-header">15 Notifications</span>
+                                                <div class="dropdown-divider"></div>
+                                                <a href="#" class="dropdown-item">
+                                                    <i class="fas fa-envelope mr-2"></i> 4 new messages
+                                                    <span class="float-right text-muted text-sm">3 mins</span>
+                                                </a>
+                                                <div class="dropdown-divider"></div>
+                                                <a href="#" class="dropdown-item">
+                                                    <i class="fas fa-users mr-2"></i> 8 friend requests
+                                                    <span class="float-right text-muted text-sm">12 hours</span>
+                                                </a>
+                                                <div class="dropdown-divider"></div>
+                                                <a href="#" class="dropdown-item">
+                                                    <i class="fas fa-file mr-2"></i> 3 new reports
+                                                    <span class="float-right text-muted text-sm">2 days</span>
+                                                </a>
+                                                <div class="dropdown-divider"></div>
+                                                <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+                                            </div>
+                                        </li>-->
+
+                    <div class="top_left">
+
+
+                        <a class="nav-link" style ="Color:Black" href="{{ url('/logout') }}"
+                           onclick="event.preventDefault();
+    document.getElementById('logout-form').submit();">
+                            Logout
                         </a>
-                        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                            <a href="#" class="dropdown-item">
-                                 Message Start 
-                                <div class="media">
-                                    <img src="{{ asset('adminLte/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle') }}">
-                                    <div class="media-body">
-                                        <h3 class="dropdown-item-title">
-                                            Brad Diesel
-                                            <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                                        </h3>
-                                        <p class="text-sm">Call me whenever you can...</p>
-                                        <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                    </div>
-                                </div>
-                                 Message End 
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a href="#" class="dropdown-item">
-                                 Message Start 
-                                <div class="media">
-                                    <img src="{{ asset('adminLte/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3') }}">
-                                    <div class="media-body">
-                                        <h3 class="dropdown-item-title">
-                                            John Pierce
-                                            <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                                        </h3>
-                                        <p class="text-sm">I got your message bro</p>
-                                        <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                    </div>
-                                </div>
-                                 Message End 
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a href="#" class="dropdown-item">
-                                 Message Start 
-                                <div class="media">
-                                    <img src="{{ asset('adminLte/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3') }}">
-                                    <div class="media-body">
-                                        <h3 class="dropdown-item-title">
-                                            Nora Silvester
-                                            <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                                        </h3>
-                                        <p class="text-sm">The subject goes here</p>
-                                        <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                    </div>
-                                </div>
-                                 Message End 
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-                        </div>
-                    </li>
-                     Notifications Dropdown Menu 
-                    <li class="nav-item dropdown">
-                        <a class="nav-link" data-toggle="dropdown" href="#">
-                            <i class="far fa-bell"></i>
-                            <span class="badge badge-warning navbar-badge">15</span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                            <span class="dropdown-item dropdown-header">15 Notifications</span>
-                            <div class="dropdown-divider"></div>
-                            <a href="#" class="dropdown-item">
-                                <i class="fas fa-envelope mr-2"></i> 4 new messages
-                                <span class="float-right text-muted text-sm">3 mins</span>
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a href="#" class="dropdown-item">
-                                <i class="fas fa-users mr-2"></i> 8 friend requests
-                                <span class="float-right text-muted text-sm">12 hours</span>
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a href="#" class="dropdown-item">
-                                <i class="fas fa-file mr-2"></i> 3 new reports
-                                <span class="float-right text-muted text-sm">2 days</span>
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-                            <i class="fas fa-th-large"></i>
-                        </a>
-                    </li>
-                </ul>-->
+
+                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+
+                </ul>
             </nav>
             <!-- /.navbar -->
             <!-- Main Sidebar Container -->
@@ -277,7 +287,7 @@
                                 <a href="{{ url('admin/special-events') }}" class="nav-link">
                                     <i class="nav-icon fas fa-th"></i>
                                     <p>
-                                       Special Events
+                                        Special Events
                                         <span class="badge badge-info right">{{$special = DB::table('special_events')->count()}}</span>
                                     </p>
                                 </a>
@@ -295,28 +305,28 @@
             table tr th:first-child {width: 50px !important;}
         </style>
         <script>
-var toggle = true;
+            var toggle = true;
 
-$(".sidebar-icon").click(function () {
-    if (toggle)
-    {
-        $(".page-container").addClass("sidebar-collapsed").removeClass("sidebar-collapsed-back");
-        $("#menu span").css({"position": "absolute"});
-    } else
-    {
-        $(".page-container").removeClass("sidebar-collapsed").addClass("sidebar-collapsed-back");
-        setTimeout(function () {
-            $("#menu span").css({"position": "relative"});
-        }, 400);
-    }
+            $(".sidebar-icon").click(function () {
+                if (toggle)
+                {
+                    $(".page-container").addClass("sidebar-collapsed").removeClass("sidebar-collapsed-back");
+                    $("#menu span").css({"position": "absolute"});
+                } else
+                {
+                    $(".page-container").removeClass("sidebar-collapsed").addClass("sidebar-collapsed-back");
+                    setTimeout(function () {
+                        $("#menu span").css({"position": "relative"});
+                    }, 400);
+                }
 
-    toggle = !toggle;
-});
-$("#menu li a").each(function () {
-    if ((window.location.href.indexOf($(this).attr('href'))) > -1) {
-        $(this).parent().addClass('active');
-    }
-});
+                toggle = !toggle;
+            });
+            $("#menu li a").each(function () {
+                if ((window.location.href.indexOf($(this).attr('href'))) > -1) {
+                    $(this).parent().addClass('active');
+                }
+            });
         </script>
         <!-- jQuery -->
         <script src="{{ asset('adminLte/plugins/jquery/jquery.min.js') }}"></script>
@@ -324,7 +334,7 @@ $("#menu li a").each(function () {
         <script src="{{ asset('adminLte/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
         <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
         <script>
-$.widget.bridge('uibutton', $.ui.button)
+            $.widget.bridge('uibutton', $.ui.button)
         </script>
         <!-- Bootstrap 4 -->
         <script src="{{ asset('adminLte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
