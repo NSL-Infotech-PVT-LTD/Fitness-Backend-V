d991a6d973fb61a0ecf7e4585b6561b362520f44
root <root@Gaurav_labs-NetScape_Labs.localdomain>
2020-06-01 10:10:39 +0530

    api

--- resources/views/admin/activity-plan/form.blade.php
+++ resources/views/admin/activity-plan/form.blade.php
@@ -10,7 +10,7 @@
 </div>
 <div class="form-group{{ $errors->has('image') ? 'has-error' : ''}}">
     {!! Form::label('image', 'Image', ['class' => 'control-label']) !!}
-    {!! Form::text('image', null, ('required' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!}
+    {!! Form::file('image', null, ('required' == 'required') ? ['class' => 'form-control', 'required' => 'required'] : ['class' => 'form-control']) !!}
     {!! $errors->first('image', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group{{ $errors->has('description') ? 'has-error' : ''}}">
