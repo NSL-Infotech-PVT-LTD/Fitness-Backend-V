3c3a9c531e9b1979be52d9029676586098135e15
root <root@Gaurav_labs-NetScape_Labs.localdomain>
2020-06-02 20:58:30 +0530

    api

--- resources/views/admin/users/show/superadmin.blade.php
+++ resources/views/admin/users/show/superadmin.blade.php
@@ -38,18 +38,16 @@
                             <tbody>
                                 <tr><th>ID.</th><td>{{ $user->id }}</td></tr>
                                 <tr><th>First Name</th><td> {{ $user->first_name }} </td></tr>
-                                <tr><th>Middle Name</th><td> {{ $user->middle_name }} </td></tr>
                                 <tr><th>Last Name</th><td> {{ $user->last_name }} </td></tr>
-                                <tr><th>Child</th><td> {{ $user->child }} </td></tr>
-                                <tr><th>Mobile</th><td> {{ $user->mobile }} </td></tr>
-                                <tr><th>Emergency Contact No</th><td> {{ $user->emergency_contact_no }} </td></tr>
                                 <tr><th>Email</th><td> {{ $user->email }} </td></tr>
-                                <tr><th>Birth Date</th><td> {{ $user->birth_date }} </td></tr>
-                                <tr><th>Marital Status</th><td> {{ $user->marital_status }} </td></tr>
-                                <tr><th>Designation</th><td> {{ $user->designation }} </td></tr>
-                                <tr><th>Emirates Id</th><td> {{ $user->emirates_id }} </td></tr>
-                                <tr><th>Address</th><td> {{ $user->address }} </td></tr>
-                                <tr><th>Status</th><td> {{ $user->status }} </td></tr>
+                                <tr>
+                                    <th>Status</th>
+                                    <?php if (!empty($user->status == '1')) { ?>
+                                        <td> Active </td>
+                                    <?php } else { ?>
+                                        <td> Inactive</td>
+                                    <?php } ?>
+                                </tr>
                                 <tr><th>Created At</th><td> {{ $user->created_at }} </td></tr>
                             </tbody>
                         </table>
