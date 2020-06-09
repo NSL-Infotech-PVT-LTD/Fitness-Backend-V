cd315927250c40e93e9e9b7bff24fcc2bb7e90fb
ergauravsethi376 <gaurav@netscapelabs.com>
2020-06-09 15:21:22 +0530

    push to live

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -26,19 +26,19 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                          
-                <div class="card-body">
-                    <?php if (isset($role_id)): ?>
-                            <a href="{{ route('user.create.withrole',$role_id) }}" class="btn btn-success btn-sm" title="Add New User">
-                                <i class="fa fa-plus" aria-hidden="true"></i> Add New
-                            </a>
-                        <?php endif; ?>
-                    
-                  
-                   
 
-                </div>
-                            
+                            <div class="card-body">
+                                <?php if (isset($role_id)): ?>
+                                    <a href="{{ route('user.create.withrole',$role_id) }}" class="btn btn-success btn-sm" title="Add New User">
+                                        <i class="fa fa-plus" aria-hidden="true"></i> Add New
+                                    </a>
+                                <?php endif; ?>
+
+
+
+
+                            </div>
+
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body">
@@ -74,75 +74,76 @@
 
 <script type="text/javascript">
     $(function () {
-    var table = $('.data-table').DataTable({
-    processing: true,
-            serverSide: true,
-            ajax: "{{ route('users-role',$role_id) }}",
-            columns: [
-            {data: 'id', name: 'id'},
+        var table = $('.data-table').DataTable({
+        processing: true,
+                serverSide: true,
+                ajax: "{{ route('users-role',$role_id) }}",
+                columns: [
+                {data: 'id', name: 'id'},
 <?php foreach ($rules as $rule): ?>
     <?php if ($rule == 'email'): ?>
-                    {data: 'email', name: 'email', orderable: false, searchable: false},
+                        {data: 'email', name: 'email', orderable: false, searchable: false},
     <?php else: ?>
-                    {data: "{{$rule}}", name: "{{$rule}}"},
+                        {data: "{{$rule}}", name: "{{$rule}}"},
     <?php endif; ?>
 <?php endforeach; ?>
-            {data: 'action', name: 'action', orderable: false, searchable: false}
-            ,
-            ]
+                {data: 'action', name: 'action', orderable: false, searchable: false
+                }
+                ,
+                ]
     });
 //deleting data
     $('.data-table').on('click', '.btnDelete[data-remove]', function (e) {
-    e.preventDefault();
-    var url = $(this).data('remove');
-    swal.fire({
-    title: "Are you sure want to remove this item?",
+        e.preventDefault();
+        var url = $(this).data('remove');
+        swal.fire({
+            title: "Are you sure want to remove this item?",
             text: "Data will be Temporary Deleted!",
             type: "warning",
             showCancelButton: true,
             confirmButtonClass: "btn-danger",
             confirmButtonText: "Confirm",
             cancelButtonText: "Cancel",
-    }).then((result) => {
-    Swal.showLoading();
-    if (result.value) {
-    $.ajax({
-    url: url,
-            type: 'DELETE',
-            dataType: 'json',
-            data: {method: '_DELETE', submit: true, _token: '{{csrf_token()}}'},
-            success: function (data) {
-            if (data == 'Success') {
-            swal.fire("Deleted!", "User has been deleted", "success");
-            table.ajax.reload(null, false);
-            }
-            }
-    });
-    }
-    });
-    });
-   $('.data-table').on('click', '.changeStatus', function (e) {
-//       alert('a');
-    e.preventDefault();
-            var id = $(this).attr('data-id');
-            var status = $(this).attr('data-status');
-            Swal.fire({
-            title: 'Are you sure you wanted to change status?',
-                    text: "You won't be able to revert this!",
-                    type: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Yes, ' + status + ' it!'
-            }).then((result) => {
-    Swal.showLoading();
+        }).then((result) => {
+            Swal.showLoading();
             if (result.value) {
-    var form_data = new FormData();
-            form_data.append("id", id);
-            form_data.append("status", status);
-            form_data.append("_token", $('meta[name="csrf-token"]').attr('content'));
-            $.ajax({
-            url: "{{route('user.changeStatus')}}",
+                $.ajax({
+                    url: url,
+                    type: 'DELETE',
+                    dataType: 'json',
+                    data: {method: '_DELETE', submit: true, _token: '{{csrf_token()}}'},
+                    success: function (data) {
+                        if (data == 'Success') {
+                            swal.fire("Deleted!", "User has been deleted", "success");
+                            table.ajax.reload(null, false);
+                        }
+                    }
+                });
+            }
+        });
+    });
+    $('.data-table').on('click', '.changeStatus', function (e) {
+//       alert('a');
+        e.preventDefault();
+        var id = $(this).attr('data-id');
+        var status = $(this).attr('data-status');
+        Swal.fire({
+            title: 'Are you sure you wanted to change status?',
+            text: "You won't be able to revert this!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, ' + status + ' it!'
+        }).then((result) => {
+            Swal.showLoading();
+            if (result.value) {
+                var form_data = new FormData();
+                form_data.append("id", id);
+                form_data.append("status", status);
+                form_data.append("_token", $('meta[name="csrf-token"]').attr('content'));
+                $.ajax({
+                    url: "{{route('user.changeStatus')}}",
                     method: "POST",
                     data: form_data,
                     contentType: false,
@@ -153,19 +154,34 @@
                     },
                     success: function (data)
                     {
-                    Swal.fire(
-                            status + ' !',
-                            'User has been ' + status + ' .',
-                            'success'
-                            ).then(() => {
-                    table.ajax.reload(null, false);
-                    });
+
+                        console.log(data);
+                        console.log(data.success);
+                        if (data.success == false) {
+
+                            Swal.fire(
+                                    "Customer can't set to active state!",
+                                    data.message,
+                                    'info'
+                                    ).then(() => {
+                                table.ajax.reload(null, false);
+                            });
+                        } else {
+
+                            Swal.fire(
+                                    status + ' !',
+                                    'User has been ' + status + ' .',
+                                    'success'
+                                    ).then(() => {
+                                table.ajax.reload(null, false);
+                            });
+                        }
                     }
-            });
-    }
+                });
+            }
+        });
     });
-    });
-   $('.data-table').on('click', '.sendPayment', function (e) {
+    $('.data-table').on('click', '.sendPayment', function (e) {
 //       alert('a');
     e.preventDefault();
             var id = $(this).attr('data-id');
@@ -209,7 +225,8 @@
     }
     });
     });
-    });
+    }
+    );
 
 </script>
 @endsection
