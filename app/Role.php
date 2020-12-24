8e238389cb876c7cb4483310ad4d00fd4890e9af
ergauravsethi376 <gaurav@netscapelabs.com>
2020-12-24 13:05:23 +0530

    first

--- app/Role.php
+++ app/Role.php
@@ -115,7 +115,7 @@ class Role extends Model {
             if ($model->isEmpty() !== true):
                 $a = [];
                 foreach ($model as $m):
-                    $a[$m->fee_type] = ['id' => $m->id, 'fee' => $m->fee];
+                    $a[$m->fee_type] = ['id' => $m->id, 'fee' => $m->fee, 'fee_type' => $m->fee_type];
                 endforeach;
 //                dd($a);
                 return $a;
