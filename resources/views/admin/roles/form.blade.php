894cd4efb770f1f6c869f330add2d7b5e0955aa2
ergauravsethi376 <gaurav@netscapelabs.com>
2021-01-12 17:58:46 +0530

    first

--- resources/views/admin/roles/form.blade.php
+++ resources/views/admin/roles/form.blade.php
@@ -85,7 +85,7 @@
 <?php // dd($role->plans);?>
 <div class="form-group{{ $errors->has('name') ? ' has-error' : ''}}">
     {!! Form::label('name', 'Name: ', ['class' => 'control-label']) !!}
-    {!! Form::select('name', ['' => 'Select Role', 'Gym Members' => 'Gym Members', 'Pool and beach members' => 'Pool and beach members', 'Local Guest' => 'Local Guest', 'Fairmont Hotel Guest' => 'Fairmont Hotel Guest'], null, ['class' => 'form-control']) !!}
+    {!! Form::select('name', ['' => 'Select Role', 'Gym Members' => 'Gym Members', 'Pool and Beach members' => 'Pool and Beach members', 'Local Guest' => 'Local Guest', 'Fairmont Hotel Guest' => 'Fairmont Hotel Guest'], null, ['class' => 'form-control']) !!}
     {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group{{ $errors->has('label') ? ' has-error' : ''}}">
