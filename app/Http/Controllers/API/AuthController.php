1d7ccc013c68f6b339f2a943f143add89cbbfd04
ergauravsethi376 <gaurav@netscapelabs.com>
2021-02-01 17:59:21 +0530

    first

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -325,7 +325,7 @@ class AuthController extends ApiController {
             return $validateAttributes;
         endif;
         try {
-            $model = \App\User::select('id', 'first_name', 'middle_name', 'last_name', 'mobile', 'emergency_contact_no', 'email', 'password', 'birth_date', 'marital_status', 'designation', 'emirates_id', 'address', 'status', 'image', 'parent_id', 'gender', 'city', 'nationality', 'about_us')->where('id', \Auth::id());
+            $model = \App\User::select('id', 'first_name', 'middle_name', 'last_name', 'mobile', 'emergency_contact_no', 'email', 'password', 'birth_date', 'marital_status', 'designation', 'emirates_id', 'address', 'status', 'image', 'parent_id', 'gender', 'city', 'nationality', 'about_us','guest_sessions')->where('id', \Auth::id());
             return parent::success(['user' => $model->first()]);
         } catch (\Exception $ex) {
             return parent::error($ex->getMessage());
