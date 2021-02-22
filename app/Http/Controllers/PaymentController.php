4223382e2a23a413c31abea3d94453c67d3461cd
ergauravsethi376 <gaurav@netscapelabs.com>
2021-02-22 19:33:10 +0530

    code update

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -116,13 +116,12 @@ class PaymentController extends Controller {
                             endif;
                         endif;
                     endif;
-                    $bookingUpdate = \App\Booking::where('id', $bookingId);
                     $updateD = [];
                     $updateD = ['payment_status' => $order->eventName, 'payment_params' => json_encode($order)];
                     if (in_array($order->eventName, \App\Booking::$_BookingApprovedStatus)):
                         $updateD += ['status' => '1'];
                     endif;
-                    $bookingUpdate->update($updateD);
+                    \App\Booking::where('id', $bookingId)->update($updateD);
                     dd($bookingUpdate->id, $order->eventName, $order);
                 endif;
             endif;
