9d2294a5c8db99892f38674b29c60a743d4acc38
Gaurav Sethi <gauravsethi@Gauravs-MacBook-Pro.local>
2021-05-19 19:09:28 +0530

    email set

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -74,7 +74,7 @@
                         <div class="icon">
                             <i class="ion ion-bag"></i>
                         </div>
-                        <a href="{{ url('admin/users') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{ url('admin/users/role/1') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
